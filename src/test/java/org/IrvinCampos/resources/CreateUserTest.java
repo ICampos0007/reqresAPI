c92884483b442a5b10d033f124a739796c7fc0cc
Irvin <irvincampos76@gmail.com>
2025-08-19 19:59:41 -0700

    create user api reqres test

---
+++ src/test/java/org/IrvinCampos/resources/CreateUserTest.java
@@ -0,0 +1,31 @@
+package org.IrvinCampos.resources;
+
+import io.restassured.path.json.JsonPath;
+import org.IrvinCampos.pojo.CreateUserReqres;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+
+public class CreateUserTest {
+
+    @Test
+    public void CreateUserReqresTest() {
+        String requestBody = "{\n" +
+                "  \"name\": \"Cozy\",\n" +
+                "  \"job\": \"King\"\n" +
+                "}";
+       String response =  given().log().all().baseUri(utils.get("baseURL"))
+                .header("Content-Type","application/json")
+                .header("x-api-key", utils.get("apiKey"))
+                .body(requestBody)
+                .when().post("/users")
+                .then().statusCode(201).extract().asString();
+
+       System.out.println(response);
+        JsonPath jsonPath = new JsonPath(response);
+        String name = jsonPath.getString("name");
+        Assert.assertEquals("Cozy",name);
+    }
+}
