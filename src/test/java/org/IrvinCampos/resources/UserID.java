9a8e15a6912a7208de5c29292639475dbc661614
Irvin <irvincampos76@gmail.com>
2025-09-15 15:38:29 -0700

    getting using by ID test

---
+++ src/test/java/org/IrvinCampos/resources/UserID.java
@@ -0,0 +1,24 @@
+package org.IrvinCampos.resources;
+
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.json.Json;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class UserID {
+
+    @Test
+    public void getUserIDTest() {
+        String response = given().log().all().baseUri(utils.get("baseURL"))
+                .header("Content-Type","application/json")
+                .header("x-api-key", utils.get("apiKey"))
+                .when().get("https://reqres.in/api/users/7")
+                .then().statusCode(200).extract().asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        System.out.println(response);
+        int id = jsonPath.getInt("data.id");
+        System.out.println(id);
+    }
+}
