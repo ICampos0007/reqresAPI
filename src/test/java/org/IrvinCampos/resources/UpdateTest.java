b5ccc946540992988c1186f323fb38e4b42e2565
Irvin <irvincampos76@gmail.com>
2025-09-23 00:05:47 -0700

    finished updated user test

--- src/test/java/org/IrvinCampos/resources/UpdateTest.java
+++ src/test/java/org/IrvinCampos/resources/UpdateTest.java
@@ -2,10 +2,22 @@ package org.IrvinCampos.resources;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class UpdateTest {
 
     @Test
     public void updateUserTest() {
-
+        String response = given().log().all().baseUri(utils.get("baseURL"))
+                .header("Content-Type","application/json")
+                .header("x-api-key", utils.get("apiKey"))
+                .body("{\n" +
+                        "  \"name\": \"morpheus\",\n" +
+                        "  \"job\": \"zion resident\",\n" +
+                        "  \"updatedAt\": \"2025-09-23T07:03:27.139Z\"\n" +
+                        "}")
+                .when().patch("/api/users/2")
+                .then().statusCode(200).extract().asString();
+        System.out.println(response);
     }
 }
